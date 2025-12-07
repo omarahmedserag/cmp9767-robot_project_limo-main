852d0b44ab3e23ccadbc773909f4d19ebb42a895
Yomar333 <omarseraj17@gmail.com>
2025-12-07 21:56:00 +0000

    i have added yolo v3 darknet

--- src/perception/setup.py
+++ src/perception/setup.py
@@ -18,7 +18,7 @@ setup(
     description='Colour detection for CMP9767 LIMO',
     license='TODO',
     tests_require=['pytest'],
-    entry_points={
+entry_points = {
     'console_scripts': [
         'detector_basic = perception.detector_basic:main',
         'controller_vision = perception.controller_vision:main',
